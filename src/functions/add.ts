a48e3acb413537db2b437855a9a7389ef970e605
Matheus Michalski Puel <matheuspuel@gmail.com>
2022-04-25 09:08:16 -0300

    document add function

--- src/functions/add.ts
+++ src/functions/add.ts
@@ -1,5 +1,27 @@
-export function add(value1: number): (value: number) => number
+/**
+ * Adds two numbers.
+ *
+ * @param value1 The first number to add.
+ * @param value2 The second number to add.
+ * @return Returns the sum.
+ */
 export function add(value1: number, value2: number): number
+/**
+ * (Curried function)
+ * Adds two numbers.
+ *
+ * @param value1 The first number to add.
+ * @return A function that takes the second number to add and then returns the sum.
+ */
+export function add(value1: number): //
+/**
+ * (Curried function)
+ * Adds two numbers.
+ *
+ * @param value The second number to add.
+ * @return Returns the sum.
+ */
+(value: number) => number
 export function add(
   value1: number,
   value2?: number,
